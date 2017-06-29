00b9146b7c553f705a2029163c281c63436dac4a
lgants <lgants@gmail.com>
2017-06-28 20:17:18 -0500

    added two validation tests

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({ name: 'Joe' });
+    joe = new User({ name: 'Joe', postCount: 0 });
     joe.save()
       .then(() => done());
   });
@@ -49,5 +49,17 @@ describe('Updating records', () => {
       User.findByIdAndUpdate(joe._id, { name: 'Alex' }),
       done
     )
+  });
+
+  it('a user can have their post count incremented by 1', (done) => {
+    // inc is an update operator
+    // operator keys passed objects containing the attribute and relevant amount
+    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        // need to assert with user since local var joe not automatically updated
+        assert(user.postCount === 1);
+        done();
+      });
   });
 });
