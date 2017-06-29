b7173243d388c7c8224d7fe44cc819c1d5b427e2
lgants <lgants@gmail.com>
2017-06-28 23:00:04 -0500

    added subdocument test

--- users/test/subdocument_test.js
+++ users/test/subdocument_test.js
@@ -33,12 +33,27 @@ describe('Subdocuments', () => {
       .then((user) => {
         assert(user.posts[0].title == 'New Post');
         done();
+      });
+  });
+
+  it('can remove an existing subdocument', (done) => {
+    const joe = new User({
+      name: 'Joe',
+      posts: [{ title: 'New Title' }]
+     });
+
+     joe.save()
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        const post = user.posts[0];
+        // calling 'remove' on a subdocument, unlike the parent document, doesn't automatically persist the removal
+        post.remove();
+        return user.save();
       })
-
-
-
-
-
-
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then((user) => {
+        assert(user.posts.length === 0);
+        done();
+      })
   });
 });
