e34bf11b670372a7c7dd4afed41752e071c00bd4
lgants <lgants@gmail.com>
2017-06-29 22:01:06 -0500

    added blogPost schema

---
+++ users/src/blogPost.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// comments uses a configuration object inside an array
+// ref is matched up against the model definition
+const BlogPostSchema = new Schema({
+  title: String,
+  content: String,
+  comments: [{
+    type: Schema.Type.ObjectId,
+    ref: 'Comment'
+  }]
+})
+
+const BlogPost = mongoose.model('blogPost', BlogPostSchema);
+
+module.exports = BlogPost;
