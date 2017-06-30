92344f96464425ab6e4ffe2f8be1d3432c59cf3b
lgants <lgants@gmail.com>
2017-06-29 23:35:55 -0500

    added first finalized association test

--- users/test/association_test.js
+++ users/test/association_test.js
@@ -14,8 +14,20 @@ describe('associations', () => {
     // mongoose will recognize pushing in an entire model and automatically creates that association despite the association using objectId
     joe.blogPosts.push(blogPost);
     blogPost.comments.push(comment);
-    // mongoose magic provides a setter on this assignment 
+    // mongoose magic provides a setter on this assignment
     comment.user = joe;
+
+    // Promise.all allows you to combine promises
+    Promise.all([joe.save(), blogPost.save(), comment.save()])
+      .then(() => done());
   });
 
+  it('saves a relation between a user and a blogpost', (done) => {
+    User.findOne({ name: 'Joe' })
+      .populate('blogPosts')
+      .then((user) => {
+        assert(user.blogPosts[0].title == 'JS is great');
+        done();
+      });
+  })
 })
