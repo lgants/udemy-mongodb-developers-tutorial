844f7c7baced5855103b784c5a26acc33a2fb0af
lgants <lgants@gmail.com>
2017-07-03 23:34:57 -0500

    added create route

---
+++ muber/test/drivers_controller_test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const request = require('supertest');
+const app = require('../app');
+const mongoose = require('mongoose');
+const Driver = mongoose.model('driver');
+
+describe('drivers controller', () => {
+  it('post to /api/drivers creates a new driver', done => {
+    Driver.count().then(count => {
+      request(app)
+        .post('/api/drivers')
+        .send({ email: 'test@test.com' })
+        .end(() => {
+          Driver.count().then(newCount => {
+            assert(count + 1 === newCount);
+          })
+          done();
+        })
+    })
+  });
+})
